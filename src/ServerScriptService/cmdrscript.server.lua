e356fb95c3bd8e53a566970968b1382b1a9699ef
BossFreind1234 <vincentredtube@gmail.com>
2022-04-02 18:10:01 -0400

    setting up cmdr(wip)

---
+++ src/ServerScriptService/cmdrscript.server.lua
@@ -0,0 +1,5 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Cmdr = require(ServerScriptService.Cmdr)
+
+Cmdr:RegisterDefaultCommands()
---
+++ src/StarterPlayer/StarterCharacterScripts/cmdr.client.lua
@@ -0,0 +1,4 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Cmdr = require(ReplicatedStorage:WaitForChild("CmdrClient"))
+
+Cmdr:SetActivationKeys({ Enum.KeyCode.F2 })
