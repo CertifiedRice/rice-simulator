ca955718ac65fcc1ea9e87bd107bc32b2c2213dc
BossFreind1234 <vincentredtube@gmail.com>
2022-04-03 13:41:52 -0400

    rebirths

--- src/StarterGui/Rebirth/rebirthclient.client.lua
+++ src/StarterGui/Rebirth/rebirthclient.client.lua
@@ -1,5 +1,18 @@
 local event = game.ReplicatedStorage.RebirthEvent
+local text = script.Parent.Frame.TextLabel
+
+local player = game.Players.LocalPlayer
 
 script.Parent.Frame.rebirthButton.MouseButton1Click:Connect(function()
-    event:FireServer()
+    -- event:FireServer()
+
+    if player.leaderstats.cash.Value >= 10000 then
+        event:FireServer(player)
+    else
+		text.Text = "You don't have enough cash!!!"
+		text.TextColor3 = Color3.fromRGB(255, 0, 0)
+       wait(3)
+		text.Text = "Requires 10,000 Cash to rebirth!"
+		text.TextColor3 = Color3.fromRGB(255, 255, 255)
+    end
 end)
